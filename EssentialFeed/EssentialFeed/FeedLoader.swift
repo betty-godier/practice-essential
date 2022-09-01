3f19bd5d49119b1a4ecbc27c404cd7032a7a14ec
Betty Godier <betty.godier@gmail.com>
2022-09-01 08:34:53 +0200

    Create the FeedLoader protocol boundary

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//      2022  Betty.dev 
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
