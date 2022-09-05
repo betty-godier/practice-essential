0cea8d0b89251bc61f224da299e881c53b817ee0
Betty Godier <betty.godier@gmail.com>
2022-09-05 21:54:47 +0200

    Move HTTPClient to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//      2022  Betty.dev 
+//
+
+import Foundation
+
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void )
+}
