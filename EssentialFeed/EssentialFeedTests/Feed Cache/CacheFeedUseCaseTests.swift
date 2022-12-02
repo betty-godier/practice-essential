d6f65f4c84c702ec28732ef32038ec426f692ff6
Betty Godier <betty.godier@gmail.com>
2022-12-02 12:17:09 +0100

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -2,16 +2,26 @@
 //      2022  Betty.dev 
 //
 
+import EssentialFeed
 import XCTest
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -19,6 +29,27 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [UniqueItem(), UniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func UniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(),
+                        description: "any",
+                        location: "any",
+                        imageURL: anyURL())
+    }
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
 
