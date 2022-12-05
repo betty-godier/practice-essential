978d610e528053eec228efac47114cb246806bbc
Betty Godier <betty.godier@gmail.com>
2022-12-05 10:16:57 +0100

    Move RemoteFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,12 @@
+//
+//      2022  Betty.dev 
+//
+
+import Foundation
+
+struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
