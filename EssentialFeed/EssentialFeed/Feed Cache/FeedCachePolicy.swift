42f7f6ecae03c017aff56379df706594577c1b4d
Betty Godier <betty.godier@gmail.com>
2022-12-10 16:50:00 +0100

    Move FeedCachePolicy to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,18 @@
+//
+//      2022  Betty.dev 
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private init() { }
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAge: Int {
+        return 7
+    }
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAge, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
